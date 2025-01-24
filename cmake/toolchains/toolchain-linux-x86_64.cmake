edf5363ed3024d4b4d97eec2ac4244a503b07238
Brian Batista <brianbatista93@gmail.com>
2025-01-24 13:56:49 -0300

    Update toolchains

--- cmake/toolchains/toolchain-linux-x86_64.cmake
+++ cmake/toolchains/toolchain-linux-x86_64.cmake
@@ -1,20 +1,5 @@
-# toolchain-arm64.cmake
+# toolchain-x86_64.cmake
 
 # Specify the target system
 set(CMAKE_SYSTEM_NAME Linux)
-set(CMAKE_SYSTEM_PROCESSOR aarch64)
-
-# Specify the compiler
-set(CMAKE_C_COMPILER aarch64-linux-gnu-gcc)
-set(CMAKE_CXX_COMPILER aarch64-linux-gnu-g++)
-
-# Specify the location of the sysroot (optional, for libraries and headers)
-# Replace <path-to-sysroot> with your actual sysroot directory, if needed.
-# set(CMAKE_SYSROOT /path/to/sysroot)
-
-# Other optional settings
-set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
-set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
-set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
-
-set(CPACK_DEBIAN_PACKAGE_ARCHITECTURE arm64)
+set(CMAKE_SYSTEM_PROCESSOR x86_64)
